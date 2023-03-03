bf9931dcc7008016260cf8ecac5718f9f81a4937
daniel amodu <danielolayinka97@gmail.com>
2023-03-03 07:45:30 +0100

    .

--- englishdic/models.py
+++ englishdic/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Dic(models.Model):
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True,)
     word = models.CharField(max_length=255,null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
 
